c8f1ba9b8ee152e5eaaf44c91e239301095ef216
Willi Ye <williye97@gmail.com>
2016-03-15 21:13:47 +0100

    input: swap hw back with recents button [2/3]
    Change-Id: I602244202452aee3306e96b0eef35b6029d46a9c
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -92,6 +92,9 @@ struct InputReaderConfiguration {
         // Volume keys rotation option changed.
         CHANGE_VOLUME_KEYS_ROTATION = 1 << 10,
 
+        // Swap keys changed.
+        CHANGE_SWAP_KEYS = 1 << 20,
+
         // All devices must be reopened.
         CHANGE_MUST_REOPEN = 1 << 31,
     };
@@ -189,6 +192,9 @@ struct InputReaderConfiguration {
     // 0 - disabled, 1 - phone or hybrid rotation mode, 2 - tablet rotation mode
     int volumeKeysRotationMode;
 
+    // Swap back with recents button
+    bool swapKeys;
+
     InputReaderConfiguration() :
             virtualKeyQuietTime(0),
             pointerVelocityControlParameters(1.0f, 500.0f, 3000.0f, 3.0f),
@@ -206,6 +212,7 @@ struct InputReaderConfiguration {
             pointerGestureMovementSpeedRatio(0.8f),
             pointerGestureZoomSpeedRatio(0.3f),
             showTouches(false),
+            swapKeys(false),
             volumeKeysRotationMode(0) { }
 
     bool getDisplayViewport(ViewportType viewportType, const String8* displayId,
@@ -1113,6 +1120,8 @@ private:
     int32_t mRotationMapOffset; // determines if and how volume keys rotate
     int32_t mOrientation; // orientation for dpad and volume keys
 
+    bool mSwapKeys; // swap back with recents button
+
     Vector<KeyDown> mKeyDowns; // keys that are down
     int32_t mMetaState;
     nsecs_t mDownTime; // time of most recent key down
@@ -1143,6 +1152,8 @@ private:
     void processKey(nsecs_t when, bool down, int32_t scanCode, int32_t usageCode);
 
     bool updateMetaStateIfNeeded(int32_t keyCode, bool down);
+
+    int getAdjustedKeyCode(int keyCode);
 
     ssize_t findKeyDown(int32_t scanCode);
 
